2ed940aadbe0179647f717647ee260a408b149c9
Malte Aschermann <masc@tu-clausthal.de>
2017-12-14 13:50:35 +0100

    minifier

--- data/minifyhtml.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-TMP=$(mktemp)
-html-minifier --case-sensitive --collapse-boolean-attributes --collapse-whitespace --decode-entities --html5 --minify-css --minify-js --remove-comments --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --trim-custom-fragments --use-short-doctype -o $TMP $1
-mv -f $TMP $1
